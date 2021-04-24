9e12c88ce103ad3c32b74c0ba08cae4eb24185f4
peterjsc <pchor90@gmail.com>
2021-04-24 16:08:04 +0100

    refactoring tests

--- calculate/pack_test.go
+++ calculate/pack_test.go
@@ -6,67 +6,71 @@ import (
 	"testing"
 )
 
-var (
-	itemTests = []int{250, 500, 1000, 2000, 5000}
-)
-
 func TestCalculatePacks(t *testing.T) {
+	itemTests := []int{250, 500, 1000, 2000, 5000}
 
-	t.Run("Happy Days, 1 item ordered", func(t *testing.T) {
-		want := []application.ItemPacksOrder{
-			application.ItemPacksOrder{"250 Items", 1},
-		}
-		got := CalcItemsWanted(1, itemTests)
+	tests := []struct {
+		name     string
+		ordered  int
+		expected []application.ItemPacksOrder
+	}{
+		{
+			name:    "Happy Days, 1 item ordered",
+			ordered: 1,
+			expected: []application.ItemPacksOrder{
+				{
+					ItemPack:       "250 Items",
+					NumberItemPack: 1,
+				},
+			},
+		},
+		{
+			name:    "Happy Days, 1 item ordered",
+			ordered: 250,
+			expected: []application.ItemPacksOrder{
+				{
+					ItemPack:       "250 Items",
+					NumberItemPack: 1,
+				},
+			},
+		},
+		{
+			name:    "Happy Days, 251 items ordered",
+			ordered: 251,
+			expected: []application.ItemPacksOrder{
+				{
+					ItemPack:       "500 Items",
+					NumberItemPack: 1,
+				},
+			},
+		},
+		{
+			name:    "Happy Days, 501 items ordered",
+			ordered: 501,
+			expected: []application.ItemPacksOrder{
+				{ItemPack: "500 Items", NumberItemPack: 1},
+				{ItemPack: "250 Items", NumberItemPack: 1},
+			},
+		},
+		{
+			name:    "Happy Days, 12001 items ordered",
+			ordered: 12001,
+			expected: []application.ItemPacksOrder{
+				{ItemPack: "5000 Items", NumberItemPack: 2},
+				{ItemPack: "2000 Items", NumberItemPack: 1},
+				{ItemPack: "250 Items", NumberItemPack: 1},
+			},
+		},
+	}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Error got %v, want %v", got, want)
-		}
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want := tt.expected
+			got := CalcItemsWanted(tt.ordered, itemTests)
 
-	t.Run("Happy Days, 250 items ordered", func(t *testing.T) {
-		want := []application.ItemPacksOrder{
-			application.ItemPacksOrder{"250 Items", 1},
-		}
-		got := CalcItemsWanted(250, itemTests)
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Error got %v, want %v", got, want)
-		}
-	})
-
-	t.Run("Happy Days, 251 items ordered", func(t *testing.T) {
-		want := []application.ItemPacksOrder{
-			application.ItemPacksOrder{"500 Items", 1},
-		}
-		got := CalcItemsWanted(251, itemTests)
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Error got %v, want %v", got, want)
-		}
-	})
-
-	t.Run("Happy Days, 501 items ordered", func(t *testing.T) {
-		want := []application.ItemPacksOrder{
-			application.ItemPacksOrder{"500 Items", 1},
-			application.ItemPacksOrder{"250 Items", 1},
-		}
-		got := CalcItemsWanted(501, itemTests)
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Error got %v, want %v", got, want)
-		}
-	})
-
-	t.Run("Happy Days, 12001 items ordered", func(t *testing.T) {
-		want := []application.ItemPacksOrder{
-			application.ItemPacksOrder{"5000 Items", 2},
-			application.ItemPacksOrder{"2000 Items", 1},
-			application.ItemPacksOrder{"250 Items", 1},
-		}
-		got := CalcItemsWanted(12001, itemTests)
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Error got %v, want %v", got, want)
-		}
-	})
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("Error got %v, want %v", got, want)
+			}
+		})
+	}
 }
